266477e499c49b06d4439424ef61f774f53c4e0f
hstyi <hstyi@foxmail.com>
2025-07-26 11:08:07 +0800

    use zulu

--- src/test/java/com/github/hstyi/libssh2j/CLibTest.java
+++ src/test/java/com/github/hstyi/libssh2j/CLibTest.java
@@ -4,8 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledOnOs;
 import org.junit.jupiter.api.condition.OS;
 
+import java.io.IOException;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.net.ServerSocket;
 
 import static com.github.hstyi.libssh2j.CLib.AF_INET;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,13 +15,14 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 @DisabledOnOs({OS.WINDOWS})
 class CLibTest {
     @Test
-    void test() throws UnknownHostException {
+    void test() throws IOException {
+        final ServerSocket serverSocket = new ServerSocket(0);
         final int fd = CLib.INSTANCE.socket(AF_INET, CLib.SOCK_STREAM, 0);
         assertNotEquals(0, fd);
 
         CLib.sockaddr_in addr = new CLib.sockaddr_in();
         addr.sin_family = AF_INET;
-        addr.sin_port = Short.reverseBytes((short) 2224);
+        addr.sin_port = Short.reverseBytes((short) serverSocket.getLocalPort());
         addr.sin_addr = InetAddress.getByName("127.0.0.1").getAddress();
         addr.sin_zero = new byte[8];
 
